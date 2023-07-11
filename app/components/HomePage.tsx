aed59a0763c339be5cc179fad6f6321f60d328c3
Evan <0xIchigo@protonmail.com>
2023-07-11 17:13:21 -0400

    Get Contract Deployment Working

--- app/components/HomePage.tsx
+++ app/components/HomePage.tsx
@@ -3,15 +3,35 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState, useEffect } from "react";
-import { createWalletClient, custom } from "viem";
+import { 
+    createPublicClient,
+    createWalletClient, 
+    custom,
+    http,
+    Hash,
+    Address,
+    TransactionReceipt,
+    stringify,
+    encodePacked,
+    keccak256, 
+    toBytes,
+} from "viem";
 import { sepolia } from "viem/chains";
 import "viem/window";
+import { rpsContract } from "../contracts/rpsContract";
 
 const SEPOLIA_ID = "0xaa36a7";
 
 export default function HomePage() {
-    const [currentAccount, setCurrentAccount] = useState("");
-    const [onRightChain, setOnRightChain] = useState(false);
+    const [currentAccount, setCurrentAccount] = useState<String>("");
+    const [hash, setHash] = useState<Hash>();
+    const [receipt, setReceipt] = useState<TransactionReceipt>();
+    const [onRightChain, setOnRightChain] = useState<Boolean>(false);
+
+    const publicClient = createPublicClient({
+        chain: sepolia,
+        transport: http()
+    });
 
     const walletClient = createWalletClient({
         chain: sepolia,
@@ -21,6 +41,38 @@ export default function HomePage() {
     const handleConnect = async () => {
         const [address] = await walletClient.requestAddresses();
         setCurrentAccount(address);
+    }
+
+    const getRandomVal = () => {
+        const newArray = new Uint8Array(32);
+        crypto.getRandomValues(newArray);
+
+        // Kind of annoying that viem doesn't have a built-in function for converting to a BigInt
+        let value = BigInt(0);
+
+        for (let i = 0; i < newArray.length; i++) {
+            value <<= BigInt(8);
+            value |= BigInt(newArray[i]);
+        }
+
+        return value;
+    }
+
+    const deployRPSContract = async () => {
+        if (!currentAccount) return;
+        
+        const account = currentAccount as Address;
+        const salt = getRandomVal();
+        const playerOneTestHash = keccak256(encodePacked(["uint8", "uint256"], [1, salt]))
+
+        // @ts-ignore
+        const hash = await walletClient.deployContract({
+            ...rpsContract,
+            account,
+            args: [playerOneTestHash, "0x" /* Pass in contract address */],
+        });
+        setHash(hash);
+        console.log(hash);
     }
 
     const checkWalletConnected = async () => {
@@ -47,49 +99,70 @@ export default function HomePage() {
         }
     }
 
+    useEffect(() => {
+        ;(async () => {
+            if (hash) {
+                const receipt = await publicClient.waitForTransactionReceipt({ hash })
+                setReceipt(receipt);
+            }
+        })()
+    }, [hash]);
 
     useEffect(() => {
         checkWalletConnected();
     }, []);
 
 
-  return (
-    <main className="flex flex-col justify-center items-center p-4 max-w-5xl mx-auto">
-        <nav className="min-w-full px-4 py-1 top-0 z-10 text-white">
-          <div className="max-w-5xl mx-auto px-4 mb-10">
-              <div className="flex flex-row-reverse items-center h-16">
-                  { currentAccount 
-                  ?   <button className="rounded-lg border-white border-2 px-4 py-2 hover:text-green">
-                          Connected: {currentAccount}
-                      </button> 
-                  :   <button onClick={handleConnect} className="rounded-lg border-white border-2 px-4 py-2 hover:text-green">
-                          Connect Wallet
-                      </button>}
-              </div>
+    return (
+        <main className="flex flex-col justify-center items-center p-4 max-w-5xl mx-auto">
+            <nav className="min-w-full px-4 py-1 top-0 z-10 text-white">
+            <div className="max-w-5xl mx-auto px-4 mb-10">
+                <div className="flex flex-row-reverse items-center h-16">
+                    { currentAccount 
+                    ?   <button className="rounded-lg border-white border-2 px-4 py-2 hover:text-green">
+                            Connected: {currentAccount}
+                        </button> 
+                    :   <button onClick={handleConnect} className="rounded-lg border-white border-2 px-4 py-2 hover:text-green">
+                            Connect Wallet
+                        </button>}
+                </div>
+                </div>
+            </nav>
+            <div className="flex flex-row justify-center items-center text-5xl">
+                <h1 className="font-FinalFrontier">
+                    Rock, Paper, Scissors, Lizard, Spock
+                </h1>
             </div>
-        </nav>
-        <div className="flex flex-row justify-center items-center text-5xl">
-            <h1 className="font-FinalFrontier">
-                Rock, Paper, Scissors, Lizard, Spock
-            </h1>
-        </div>
-        {(currentAccount == "") && (
-            <div className="mt-4">
-                Please connect your wallet to play!
-            </div>
-        )}
-        {currentAccount && !onRightChain && (
-            <div className="mt-4">
-                Please switch to the Sepolia Testnet to play!
-            </div>
-        )}
-        {onRightChain && (
-            <div>
-                You are on the right chain!
-            </div>
-        )}
-    </main>
-  )
+            {(currentAccount == "") && (
+                <div className="mt-4">
+                    Please connect your wallet to play!
+                </div>
+            )}
+            {currentAccount && !onRightChain && (
+                <div className="mt-4">
+                    Please switch to the Sepolia Testnet to play!
+                </div>
+            )}
+            {currentAccount && onRightChain && !receipt && (
+                <button onClick={deployRPSContract}>
+                    Deploy Contract
+                </button>
+            )}
+            {receipt && (
+                <>
+                    <div>
+                        Contract address: {receipt.contractAddress}
+                    </div>
+                    <div>
+                        Receipt:{' '}
+                        <pre>
+                            <code>{stringify(receipt, null, 2)}</code>
+                        </pre>
+                    </div>
+                </>
+            )}
+        </main>
+    )
 }
 
 // Spock: <span className="font-Icons">v</span>
