a1042f6c1238ba733b733f4172150faa2fb4f7ca
Evan <0xIchigo@protonmail.com>
2023-07-11 15:03:37 -0400

    Make Sure the User is on the Sepolia Testnet

--- app/components/HomePage.tsx
+++ app/components/HomePage.tsx
@@ -7,14 +7,16 @@ import { createWalletClient, custom } from "viem";
 import { sepolia } from "viem/chains";
 import "viem/window";
 
+const SEPOLIA_ID = "0xaa36a7";
+
 export default function HomePage() {
+    const [currentAccount, setCurrentAccount] = useState("");
+    const [onRightChain, setOnRightChain] = useState(false);
 
-  const walletClient = createWalletClient({
-    chain: sepolia,
-    transport: custom(window.ethereum!)
-  });
-
-  const [currentAccount, setCurrentAccount] = useState("");
+    const walletClient = createWalletClient({
+        chain: sepolia,
+        transport: custom(window.ethereum!)
+    });
 
     const handleConnect = async () => {
         const [address] = await walletClient.requestAddresses();
@@ -30,14 +32,21 @@ export default function HomePage() {
                     if (accounts[0] == undefined) setCurrentAccount("");
                 })
 
+                ethereum.on("chainChanged", function (chainId: string) {
+                    if (chainId !== SEPOLIA_ID) setOnRightChain(false);
+                })
+
                 const accounts = await ethereum.request({ method: "eth_accounts"});
                 accounts.length !== 0 ? setCurrentAccount(accounts[0]) : console.log("No account found!");
+
+                (window as any).ethereum.chainId == SEPOLIA_ID ? setOnRightChain(true) : setOnRightChain(false);
 
             } else {
                 console.log("Please install Metamask to play RPSLS!");
             }
         }
     }
+
 
     useEffect(() => {
         checkWalletConnected();
@@ -47,10 +56,8 @@ export default function HomePage() {
   return (
     <main className="flex flex-col justify-center items-center p-4 max-w-5xl mx-auto">
         <nav className="min-w-full px-4 py-1 top-0 z-10 text-white">
-          <div className="max-w-5xl mx-auto px-4">
-              <div className="flex items-center justify-between h-16">
-                  <Link href="/" className="sm:text-2xl text-lg font-semibold text-white hover:text-green mr-2 no-underline">RPSLS</Link>
-                    
+          <div className="max-w-5xl mx-auto px-4 mb-10">
+              <div className="flex flex-row-reverse items-center h-16">
                   { currentAccount 
                   ?   <button className="rounded-lg border-white border-2 px-4 py-2 hover:text-green">
                           Connected: {currentAccount}
@@ -60,12 +67,27 @@ export default function HomePage() {
                       </button>}
               </div>
             </div>
-      </nav>
-      <div className="flex flex-row justify-center items-center text-5xl">
-        <h1 className="font-FinalFrontier">
-          Rock, Paper, Scissors, Lizard, Spock
-        </h1>
-      </div>
+        </nav>
+        <div className="flex flex-row justify-center items-center text-5xl">
+            <h1 className="font-FinalFrontier">
+                Rock, Paper, Scissors, Lizard, Spock
+            </h1>
+        </div>
+        {(currentAccount == "") && (
+            <div className="mt-4">
+                Please connect your wallet to play
+            </div>
+        )}
+        {currentAccount && !onRightChain && (
+            <div className="mt-4">
+                Please switch to the Sepolia Testnet to play!
+            </div>
+        )}
+        {onRightChain && (
+            <div>
+                You are on the right chain!
+            </div>
+        )}
     </main>
   )
 }
